009afa3bad6b84abfa918761577559d84f21092a
Alexander Rovang <DXRovang@gmail.com>
2022-04-25 11:48:44 -0400

    first commit

---
+++ src/Parent1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Parent1 extends React.Component{
+
+  render(){
+    return(
+      <div>
+        Hello from Parent1
+      </div>
+    )
+  }
+}
+export default Parent1
