02ba6be31f8f360eb5949df665d497f3aba1e142
Alexander Rovang <DXRovang@gmail.com>
2022-04-25 13:41:43 -0400

    adds connect and componentDidMount to Parent1

--- src/Parent1.js
+++ src/Parent1.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import Child1 from './Child1'
+import { fetchCards } from './actions/fetchCards'
+import { connect } from 'react-redux'
 
 class Parent1 extends React.Component{
+
+  componentDidMount(){
+    this.props.fetchCards
+  }
 
   render(){
     return(
       <div>
         Hello from Parent1
-        <Child1/>
+        <Child1 cards={this.props.cards}/>
       </div>
     )
+
+    const mapStateToProps = (state) =>{
+      return {
+        cards: state.cards
+      }
+    }
   }
 }
-export default Parent1
+export default connect(mapStateToProps, {fetchCards})(Parent1)
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import cardReducer from './reducers/cardReducer'
+import characterReducer from './reducers/characterReducer'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const combinedReducer = combineReducers(
